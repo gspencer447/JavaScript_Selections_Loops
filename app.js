6e9e84fbebd47b30c162018a9deae14c6bbb143e
Graham Spencer <gspencer447@yahoo.com>
2023-10-02 13:30:53 -0500

    completed exercises 1-4

--- app.js
+++ app.js
@@ -1,7 +1,76 @@
 console.log("Hello World!\n==========\n");
 
 // Exercise 1 Section
-console.log("EXERCISE 1:\n==========\n");
+for (let i = 1; i <= 100; i++){
+    if (i % 2 === 1){
+        console.log(i)
+    }
+}
 
 // Exercise 2 Section
-console.log("EXERCISE 2:\n==========\n");
+for (let i = 1; i <= 100; i++){
+    if (i % 3 === 0 && i % 5 === 0){
+        console.log([i] + "FIZZBUZZ")
+    } else if (i % 3 === 0){
+        console.log([i] + "FIZZ")
+    } else if (i % 5 === 0) {
+        console.log([i] + "BUZZ")
+    }
+}
+
+//Exercise 3 Section
+////while loops
+
+// let i = 1;
+// while (i <= 100){
+//     if (i % 2 === 1){
+//         console.log(i);
+//     }
+//     i++;
+// }
+
+// let i = 1;
+// while (i <= 100){
+//     if (i % 3 === 0 && i % 5 === 0){
+//         console.log(i + "FIZZBUZZ")
+//     } else if ([i] % 3 === 0){
+//         console.log(i + "FIZZ")
+//     } else if ([i] % 5 === 0){
+//         console.log(i + "BUZZ")
+//     }
+//     i++;
+// }
+
+////do-while loops
+// let i = 1;
+// do {
+//     if (i % 2 === 1){
+//         console.log(i);
+//     }
+//     i++;
+// } while (i <= 100);
+
+// let i = 1;
+// do {
+//     if (i % 3 === 0 && i % 5 === 0){
+//         console.log(i + "FIZZBUZZ")
+//     } else if (i % 3 === 0){
+//         console.log(i + "FIZZ")
+//     } else if (i % 5 === 0){
+//         console.log(i + "BUZZ")
+//     }
+//     i++;
+// } while (i <= 100);
+
+//Exercise 4 Section
+let value = Math.round((Math.random() * 500)); // creates a random number between 0 and 500
+let n = Math.round(Math.random() * (500 - 100) + 100); // creates a random number between 100 and 500
+
+for (let i = 0; i <= n; i++){
+    if (i === value){
+        console.log(i + "Found value!")
+        break;
+    } else {
+        console.log("Did not find value")
+    }
+}
